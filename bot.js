9f7e2451c32fe5a799405c14d7614b6d7b6faa09
Goran Mandic <godzi1991@gmail.com>
2018-05-22 20:51:52 +0200

    Dummy update

--- bot.js
+++ bot.js
@@ -61,6 +61,7 @@ var bot_options = {
   studio_command_uri: process.env.studio_command_uri
 };
 
+
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
 if (process.env.MONGODB_URI) {
