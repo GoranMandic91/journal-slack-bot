17f9f78a1753ac2d3e44f03601fc45879a61cdf9
Goran Mandic <godzi1991@gmail.com>
2018-05-24 22:57:59 +0200

    Add skill to get all users in team

---
+++ skills/get_all_users.js
@@ -0,0 +1,59 @@
+
+module.exports = (controller) => {
+
+    controller.hears(['get users'], 'direct_message', (bot, message) => {
+
+        bot.createConversation(message, (err, convo) => {
+
+            convo.addMessage({
+                text: 'I\'m getting all users, please wait for a second :simple_smile:',
+                action: 'get-all-users'
+            });
+
+            convo.addMessage({
+                text: 'Here you go :man-tipping-hand::skin-tone-2:'
+            }, 'end-conversation');
+            
+            bot.api.users.list({}, (error, response) => {
+
+                let currentDate = Date.now() / 1000;
+
+                let members = response.members.filter((member) => {
+                    return !member.is_bot && member.id !== 'USLACKBOT';
+                });
+
+                let attachments = members.map(member => {
+                    return {
+                        author_name: member.real_name,
+                        author_icon: member.profile.image_24,
+                        thumb_url: member.profile.image_72,
+                        color: member.color,
+                        fields: [
+                            {
+                                title: "e-mail",
+                                value: member.profile.email,
+                                short: true
+                            },
+                            {
+                                title: "username",
+                                value: '<@' + member.id + '>',
+                                short: true
+                            }
+                        ],
+                        footer: "Journal Bot",
+                        footer_icon: "https://platform.slack-edge.com/img/default_application_icon.png",
+                        ts: currentDate,
+                    }
+                })
+
+                convo.addMessage({
+                    attachments: attachments,
+                    action: 'end-conversation'
+                }, 'get-all-users');
+            })
+
+            convo.activate();
+
+        })
+    });
+}
